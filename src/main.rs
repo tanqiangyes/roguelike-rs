7023ac3cca06d6b18fbaf8491216c08f9910f9ac
tanqiangyes <826285820@qq.com>
2023-04-13 23:43:28 +0800

    section2.1 - Rendering - complete code

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,62 @@
-use rltk::{BError, GameState, Rltk, RltkBuilder};
+use rltk::{BError, GameState, Rltk, RltkBuilder, VirtualKeyCode, RGB};
+use specs::prelude::*;
+use specs_derive::Component;
+use std::cmp::{max, min};
 fn main() -> BError {
     let context = RltkBuilder::simple80x50()
         .with_title("roguelike game")
         .build()?;
-    let gs = State {};
+    let mut gs = State { ecs: World::new() };
+    gs.ecs.register::<Position>();
+    gs.ecs.register::<Renderable>();
+
+    gs.ecs
+        .create_entity()
+        .with(Position { x: 40, y: 25 })
+        .with(Renderable {
+            glyph: rltk::to_cp437('@'),
+            fg: RGB::named(rltk::YELLOW),
+            bg: RGB::named(rltk::BLACK),
+        })
+        .build();
+    for i in 0..10 {
+        gs.ecs
+            .create_entity()
+            .with(Position { x: i * 7, y: 20 })
+            .with(Renderable {
+                glyph: rltk::to_cp437('☺'),
+                fg: RGB::named(rltk::RED),
+                bg: RGB::named(rltk::BLACK),
+            })
+            .build();
+    }
+
     rltk::main_loop(context, gs)
 }
 
-struct State {}
+struct State {
+    ecs: World,
+}
 
 impl GameState for State {
     fn tick(&mut self, ctx: &mut Rltk) {
-        ctx.cls();
-        ctx.print(1, 1, "Hello roguelike game!");`
+        let position = self.ecs.read_storage::<Position>();
+        let renderable = self.ecs.read_storage::<Renderable>();
+        for (pos, render) in (&position, &renderable).join() {
+            ctx.set(pos.x, pos.y, render.fg, render.bg, render.glyph);
+        }
     }
+}
+
+#[derive(Component)]
+struct Position {
+    x: i32,
+    y: i32,
+}
+
+#[derive(Component)]
+struct Renderable {
+    glyph: rltk::FontCharType,
+    fg: RGB,
+    bg: RGB,
 }
